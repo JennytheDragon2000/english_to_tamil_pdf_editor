4adbe3334559ad81c5549f64835be281ef9f3033
jenny <www.srinath.25.com@gmail.com>
2024-03-02 08:11:25 -0500

    application is gong into an infinite loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import WebViewer from "@pdftron/webviewer";
+import * as PDFNet from "@pdftron/pdfnet-node";
 
 function App() {
   const viewerDiv = useRef<HTMLDivElement>(null);
@@ -12,11 +13,43 @@ function App() {
         initialDoc: "blank-Pdf.pdf",
         licenseKey:
           "demo:1708915436701:7f5048510300000000b3048899a6ad6f12681330b31c25e4316c754802",
+        fullAPI: true,
       },
-      viewerDiv.current as HTMLDivElement
+      viewerDiv.current as HTMLDivElement,
     ).then((instance) => {
-      // Enable the tools to edit the PDF content
+      // Move the enableFeatures call inside the then block
       instance.UI.enableFeatures([instance.UI.Feature.ContentEdit]);
+
+      const { documentViewer, annotationManager } = instance.Core;
+
+      documentViewer.addEventListener("documentLoaded", async () => {
+        const doc = documentViewer.getDocument();
+        const xfdfString = await annotationManager.exportAnnotations();
+        const options = { xfdfString };
+        const data = await doc.getFileData(options);
+        const arr = new Uint8Array(data);
+        const blob = new Blob([arr], { type: "application/pdf" });
+
+        // Convert the Blob to a FormData object
+        const formData = new FormData();
+        // Change "pdf" to "file" to match the expected key on the server
+        formData.append("file", blob, "filename.pdf");
+
+        fetch("http://127.0.0.1:5000/upload", {
+          method: "POST",
+          body: formData,
+        })
+          .then((response) => response.blob())
+          .then((blob) => {
+            // Create a URL for the blob
+            const url = URL.createObjectURL(blob);
+            // Load the URL into the WebViewer
+            instance.UI.loadDocument(url, {extension: 'pdf'});
+          })
+          .catch((error) => {
+            console.error("Error sending PDF:", error);
+          });
+      });
     });
   }, []);
 
