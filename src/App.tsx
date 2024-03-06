76945be1be526f3717c3ebaa263185863c873819
jenny <www.srinath.25.com@gmail.com>
2024-03-05 21:43:47 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ function App() {
             formData.append("pdf", blob, "filename.pdf");
 
             // Send the PDF to the server
-            fetch("http://127.0.0.1:5000/remove-watermark", {
+            fetch("http://127.0.0.1:8001/remove-watermark", {
               method: "POST",
               body: formData,
             })
@@ -76,45 +76,45 @@ function App() {
 
       const { documentViewer } = instance.Core;
 
-      // const handleDocumentLoaded = async () => {
-      //   const { annotationManager } = instance.Core;
-      //   const doc = documentViewer.getDocument();
-      //   const xfdfString = await annotationManager.exportAnnotations();
-      //   const options = { xfdfString };
-      //   const data = await doc.getFileData(options);
-      //   const arr = new Uint8Array(data);
-      //   const blob = new Blob([arr], { type: "application/pdf" });
-      //
-      //   const formData = new FormData();
-      //   formData.append("file", blob, "filename.pdf");
-      //
-      //   fetch("http://127.0.0.1:5000/upload", {
-      //     method: "POST",
-      //     body: formData,
-      //   })
-      //     .then((response) => response.blob())
-      //     .then((blob) => {
-      //       const url = URL.createObjectURL(blob);
-      //       instance.UI.loadDocument(url, { extension: "pdf" });
-      //     })
-      //     .catch((error) => {
-      //       console.error("Error sending PDF:", error);
-      //     });
-      //
-      //   instance.Core.documentViewer.removeEventListener(
-      //     "documentLoaded",
-      //     handleDocumentLoaded,
-      //   );
-      // };
-      //
-      // documentViewer.addEventListener("documentLoaded", handleDocumentLoaded);
+      const handleDocumentLoaded = async () => {
+        const { annotationManager } = instance.Core;
+        const doc = documentViewer.getDocument();
+        const xfdfString = await annotationManager.exportAnnotations();
+        const options = { xfdfString };
+        const data = await doc.getFileData(options);
+        const arr = new Uint8Array(data);
+        const blob = new Blob([arr], { type: "application/pdf" });
 
-      // return () => {
-      //   instance.Core.documentViewer.removeEventListener(
-      //     "documentLoaded",
-      //     handleDocumentLoaded,
-      //   );
-      // };
+        const formData = new FormData();
+        formData.append("file", blob, "filename.pdf");
+
+        fetch("http://127.0.0.1:5000/upload", {
+          method: "POST",
+          body: formData,
+        })
+          .then((response) => response.blob())
+          .then((blob) => {
+            const url = URL.createObjectURL(blob);
+            instance.UI.loadDocument(url, { extension: "pdf" });
+          })
+          .catch((error) => {
+            console.error("Error sending PDF:", error);
+          });
+
+        instance.Core.documentViewer.removeEventListener(
+          "documentLoaded",
+          handleDocumentLoaded,
+        );
+      };
+
+      documentViewer.addEventListener("documentLoaded", handleDocumentLoaded);
+
+      return () => {
+        instance.Core.documentViewer.removeEventListener(
+          "documentLoaded",
+          handleDocumentLoaded,
+        );
+      };
     });
   }, []);
 
